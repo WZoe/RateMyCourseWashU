c160389f6b87add1e69e3ac808016896c1aa48c4
sxz294 <shenxiaozhi1993@hotmail.com>
2019-11-09 12:20:17 -0600

    interface
    interface example

---
+++ RateMyCourseWashU/RateMyCourseWashU/Interface.swift
@@ -0,0 +1,13 @@
+//
+//  Interface.swift
+//  RateMyCourseWashU
+//
+//  Created by 沈晓知 on 11/9/19.
+//  Copyright © 2019 438group. All rights reserved.
+//
+
+import Foundation
+
+func login(userName: String!, password: String!) ->Bool{
+    return true
+}
