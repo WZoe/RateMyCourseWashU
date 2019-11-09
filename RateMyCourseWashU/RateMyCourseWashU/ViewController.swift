40957eeab7dbb3cd0e59a0806fa5e1c7a30ef639
wzoe <455391638@qq.com>
2019-11-09 11:25:31 -0600

    init

---
+++ RateMyCourseWashU/RateMyCourseWashU/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RateMyCourseWashU
+//
+//  Created by Zoe Wang on 11/9/19.
+//  Copyright © 2019 438group. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
