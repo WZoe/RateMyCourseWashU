db84c9865332d8a3db844435c550e322dfe63308
wzoe <455391638@qq.com>
2019-11-30 12:44:27 -0600

    added pusher dependency

---
+++ RateMyCourseWashU/RateMyCourseWashU/chatkitUtils.swift
@@ -0,0 +1,34 @@
+//
+//  chatkitUtils.swift
+//  RateMyCourseWashU
+//
+//  Created by Zoe Wang on 11/30/19.
+//  Copyright © 2019 438group. All rights reserved.
+//
+
+import Foundation
+
+func getChatkit(bundle: Bundle) -> (
+    instanceLocator: String,
+    tokenProviderEndpoint: String,
+    userId: String)? {
+        guard
+            let path = bundle.path(forResource: "Chatkit", ofType: "plist"),
+            let values = NSDictionary(contentsOfFile: path) as? [String: Any]
+            else {
+                print("Missing Chatkit.plist file with 'ChatkitInstanceLocator', 'ChatkitTokenProviderEndpoint', 'ChatkitUserId', and 'ChatkitRoomId' entries in main bundle!")
+                return nil
+        }
+        
+        guard
+            let instanceLocator = values["ChatkitInstanceLocator"] as? String,
+            let tokenProviderEndpoint = values["ChatkitTokenProviderEndpoint"] as? String,
+            let userId = cache.object(forKey: "userid") as? String
+            else {
+                print("Chatkit.plist file at \(path) is missing 'ChatkitInstanceLocator', 'ChatkitTokenProviderEndpoint', 'ChatkitUserId', and/or 'ChatkitRoomId' entries!")
+                print("File currently has the following entries: \(values)")
+                return nil
+        }
+        return (instanceLocator: instanceLocator, tokenProviderEndpoint: tokenProviderEndpoint, userId: userId)
+}
+
